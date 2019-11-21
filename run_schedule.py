18a90356981ef195e06adf87f487251fad53266c
Jose Angel Espinoza Arce <joseangel.epzarce@gmail.com>
2019-11-21 11:40:17 -0600

    first commit

---
+++ run_schedule.py
@@ -0,0 +1,137 @@
+import atexit
+import logging
+import gc
+import os
+import re
+import shutil
+import sys
+import tempfile
+import warnings
+
+from tornado.ioloop import IOLoop
+from distributed import Scheduler
+from distributed.security import Security
+from distributed.cli.utils import install_signal_handlers
+from distributed.proctitle import (
+    enable_proctitle_on_children,
+    enable_proctitle_on_current,
+)
+
+logger = logging.getLogger("distributed.scheduler")
+
+
+def main(
+    host=None,
+    port=None,
+    bokeh_port=None,
+    show=True,
+    dashboard=True,
+    bokeh=None,
+    dashboard_prefix=None,
+    use_xheaders=False,
+    pid_file="",
+    local_directory="",
+    tls_ca_file=None,
+    tls_cert=None,
+    tls_key=None,
+    dashboard_address="127.0.0.1:8788",
+    **kwargs
+):
+    g0, g1, g2 = gc.get_threshold()  # https://github.com/dask/distributed/issues/1653
+    gc.set_threshold(g0 * 3, g1 * 3, g2 * 3)
+
+    enable_proctitle_on_current()
+    enable_proctitle_on_children()
+
+    if bokeh_port is not None:
+        warnings.warn(
+            "The --bokeh-port flag has been renamed to --dashboard-address. "
+            "Consider adding ``--dashboard-address :%d`` " % bokeh_port
+        )
+        dashboard_address = bokeh_port
+    if bokeh is not None:
+        warnings.warn(
+            "The --bokeh/--no-bokeh flag has been renamed to --dashboard/--no-dashboard. "
+        )
+        dashboard = bokeh
+
+    if port is None and (not host or not re.search(r":\d", host)):
+        port = 8786
+
+    sec = Security(
+        **{
+            k: v
+            for k, v in [
+                ("tls_ca_file", tls_ca_file),
+                ("tls_scheduler_cert", tls_cert),
+                ("tls_scheduler_key", tls_key),
+            ]
+            if v is not None
+        }
+    )
+
+    if not host and (tls_ca_file or tls_cert or tls_key):
+        host = "tls://"
+
+    if pid_file:
+        with open(pid_file, "w") as f:
+            f.write(str(os.getpid()))
+
+        def del_pid_file():
+            if os.path.exists(pid_file):
+                os.remove(pid_file)
+
+        atexit.register(del_pid_file)
+
+    local_directory_created = False
+    if local_directory:
+        if not os.path.exists(local_directory):
+            os.mkdir(local_directory)
+            local_directory_created = True
+    else:
+        local_directory = tempfile.mkdtemp(prefix="scheduler-")
+        local_directory_created = True
+    if local_directory not in sys.path:
+        sys.path.insert(0, local_directory)
+
+    if sys.platform.startswith("linux"):
+        import resource  # module fails importing on Windows
+
+        soft, hard = resource.getrlimit(resource.RLIMIT_NOFILE)
+        limit = max(soft, hard // 2)
+        resource.setrlimit(resource.RLIMIT_NOFILE, (limit, hard))
+
+    loop = IOLoop.current()
+    logger.info("-" * 47)
+
+    scheduler = Scheduler(
+        loop=loop,
+        security=sec,
+        host=host,
+        port=port,
+        dashboard_address=dashboard_address if dashboard else None,
+        service_kwargs={"dashboard": {"prefix": dashboard_prefix}},
+        **kwargs
+    )
+    logger.info("Local Directory: %26s", local_directory)
+    logger.info("-" * 47)
+
+    install_signal_handlers(loop)
+
+    async def run():
+        await scheduler
+        await scheduler.finished()
+
+    try:
+        loop.run_sync(run)
+    finally:
+        scheduler.stop()
+        if local_directory_created:
+            shutil.rmtree(local_directory)
+
+        logger.info("End scheduler at %r", scheduler.address)
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ run_worker.py
@@ -0,0 +1,202 @@
+
+def main(
+    scheduler,
+    host,
+    worker_port,
+    listen_address,
+    contact_address,
+    nanny_port,
+    nthreads,
+    nprocs,
+    nanny,
+    name,
+    pid_file,
+    resources,
+    dashboard,
+    bokeh,
+    bokeh_port,
+    scheduler_file,
+    dashboard_prefix,
+    tls_ca_file,
+    tls_cert,
+    tls_key,
+    dashboard_address,
+    **kwargs
+):
+    g0, g1, g2 = gc.get_threshold()  # https://github.com/dask/distributed/issues/1653
+    gc.set_threshold(g0 * 3, g1 * 3, g2 * 3)
+
+    enable_proctitle_on_current()
+    enable_proctitle_on_children()
+
+    if bokeh_port is not None:
+        warnings.warn(
+            "The --bokeh-port flag has been renamed to --dashboard-address. "
+            "Consider adding ``--dashboard-address :%d`` " % bokeh_port
+        )
+        dashboard_address = bokeh_port
+    if bokeh is not None:
+        warnings.warn(
+            "The --bokeh/--no-bokeh flag has been renamed to --dashboard/--no-dashboard. "
+        )
+        dashboard = bokeh
+
+    sec = Security(
+        **{
+            k: v
+            for k, v in [
+                ("tls_ca_file", tls_ca_file),
+                ("tls_worker_cert", tls_cert),
+                ("tls_worker_key", tls_key),
+            ]
+            if v is not None
+        }
+    )
+
+    if nprocs > 1 and worker_port != 0:
+        logger.error(
+            "Failed to launch worker.  You cannot use the --port argument when nprocs > 1."
+        )
+        sys.exit(1)
+
+    if nprocs > 1 and not nanny:
+        logger.error(
+            "Failed to launch worker.  You cannot use the --no-nanny argument when nprocs > 1."
+        )
+        sys.exit(1)
+
+    if contact_address and not listen_address:
+        logger.error(
+            "Failed to launch worker. "
+            "Must specify --listen-address when --contact-address is given"
+        )
+        sys.exit(1)
+
+    if nprocs > 1 and listen_address:
+        logger.error(
+            "Failed to launch worker. "
+            "You cannot specify --listen-address when nprocs > 1."
+        )
+        sys.exit(1)
+
+    if (worker_port or host) and listen_address:
+        logger.error(
+            "Failed to launch worker. "
+            "You cannot specify --listen-address when --worker-port or --host is given."
+        )
+        sys.exit(1)
+
+    try:
+        if listen_address:
+            (host, worker_port) = get_address_host_port(listen_address, strict=True)
+
+        if contact_address:
+            # we only need this to verify it is getting parsed
+            (_, _) = get_address_host_port(contact_address, strict=True)
+        else:
+            # if contact address is not present we use the listen_address for contact
+            contact_address = listen_address
+    except ValueError as e:
+        logger.error("Failed to launch worker. " + str(e))
+        sys.exit(1)
+
+    if nanny:
+        port = nanny_port
+    else:
+        port = worker_port
+
+    if not nthreads:
+        nthreads = CPU_COUNT // nprocs
+
+    if pid_file:
+        with open(pid_file, "w") as f:
+            f.write(str(os.getpid()))
+
+        def del_pid_file():
+            if os.path.exists(pid_file):
+                os.remove(pid_file)
+
+        atexit.register(del_pid_file)
+
+    if resources:
+        resources = resources.replace(",", " ").split()
+        resources = dict(pair.split("=") for pair in resources)
+        resources = valmap(float, resources)
+    else:
+        resources = None
+
+    loop = IOLoop.current()
+
+    if nanny:
+        kwargs.update({"worker_port": worker_port, "listen_address": listen_address})
+        t = Nanny
+    else:
+        if nanny_port:
+            kwargs["service_ports"] = {"nanny": nanny_port}
+        t = Worker
+
+    if (
+        not scheduler
+        and not scheduler_file
+        and dask.config.get("scheduler-address", None) is None
+    ):
+        raise ValueError(
+            "Need to provide scheduler address like\n"
+            "dask-worker SCHEDULER_ADDRESS:8786"
+        )
+
+    with ignoring(TypeError, ValueError):
+        name = int(name)
+
+    nannies = [
+        t(
+            scheduler,
+            scheduler_file=scheduler_file,
+            nthreads=nthreads,
+            loop=loop,
+            resources=resources,
+            security=sec,
+            contact_address=contact_address,
+            host=host,
+            port=port,
+            dashboard_address=dashboard_address if dashboard else None,
+            service_kwargs={"dashboard": {"prefix": dashboard_prefix}},
+            name=name
+            if nprocs == 1 or name is None or name == ""
+            else str(name) + "-" + str(i),
+            **kwargs
+        )
+        for i in range(nprocs)
+    ]
+
+    async def close_all():
+        # Unregister all workers from scheduler
+        if nanny:
+            await asyncio.gather(*[n.close(timeout=2) for n in nannies])
+
+    signal_fired = False
+
+    def on_signal(signum):
+        nonlocal signal_fired
+        signal_fired = True
+        if signum != signal.SIGINT:
+            logger.info("Exiting on signal %d", signum)
+        asyncio.ensure_future(close_all())
+
+    async def run():
+        await asyncio.gather(*nannies)
+        await asyncio.gather(*[n.finished() for n in nannies])
+
+    install_signal_handlers(loop, cleanup=on_signal)
+
+    try:
+        loop.run_sync(run)
+    except TimeoutError:
+        # We already log the exception in nanny / worker. Don't do it again.
+        if not signal_fired:
+            logger.info("Timed out starting worker")
+        sys.exit(1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        logger.info("End worker")
